647dae94126ec4bf83221d9fa516e59c71387acc
JuanjoPischetz <juanjo.pischetz@gmail.com>
2022-10-21 16:54:08 -0300

    landing in progress

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -7,10 +7,14 @@ const Landing = ()=>{
 
     return(
         <div className={styles.main}>
+            <div className={styles.cover}></div>
             <video className={styles.video} src={BgVideo} autoPlay loop muted />
             <div className={styles.content}>
+            <div className={styles.text}>
+                <span className={styles.span}>Bienvenido Gamer</span>
+            </div>
             <Link to= '/home'>
-            <button className={styles.button} >Home</button>
+                <button className={styles.button}>Home</button>
             </Link>
             </div>
         </div>
